01bbb216bce170ec8c76803b064744b044467ee8
Adrian Nirmal Andrew <kingspammernerd@gmail.com>
2019-02-05 11:30:49 +0530

    Server officially finished
    -Implemented DEL
    -Did some refactoring, bringing LOC to <280
    
    Pending:
    -GUI Client

--- tls_files/server/ClientHandler.java
+++ tls_files/server/ClientHandler.java
@@ -79,7 +79,7 @@ public class ClientHandler implements Runnable {
 			in=new DataInputStream(this.sock.getInputStream());
 			out=new DataOutputStream(this.sock.getOutputStream());
 		} catch(IOException ie) {
-			printError(ie.toString());
+			printErrorAndDie(ie.toString());
 		}
 		
 		//Request handling:
@@ -91,181 +91,174 @@ public class ClientHandler implements Runnable {
 			} catch(IOException o) {
 				printErrorAndDie(o.getMessage());
 			}
-			//Convert request to uppercase. In case user logs out, exit the loop and print closing message:
-			if(req!=null)
-				req=req.toUpperCase();
-			else
-				break;
+			//If the user logs out req becomes null, so exit the loop and print closing message:
+			if(req==null) break;
+			//Convert req to uppercase:
+			req=req.toUpperCase();
 			//Handle a LOGIN request:
-			if(req.equals("LOGIN") && !isClientLoggedIn) {
-				//Check if session requires password:
-				if(this.passHash64==null)
-					try {
-						printError("Client at " + this.sock.getRemoteSocketAddress() + " logging in.");
-						this.out.writeUTF("LOGIN_OK");
-						printError("Client at " + this.sock.getRemoteSocketAddress() + " logged in.");
-						this.out.flush();
-						isClientLoggedIn=true;
-						continue;
-					} catch(IOException o1) {
-						printErrorAndDie(o1.getMessage());
-					}
-				else {
-					try {
-						this.out.writeUTF("PASSWD");
-						//Check password:
-						String p=this.in.readUTF();
-						if(this.checkPassword(p)) {
-							this.out.writeUTF("LOGIN_OK");
-							printError("Client at " + this.sock.getRemoteSocketAddress() + " logged in.");
-							isClientLoggedIn=true;
-							continue;
-						}
-						else {
-							this.out.writeUTF("LOGIN_BAD");
-							printErrorAndDie("Client at " + this.sock.getRemoteSocketAddress() + " disconnected (bad login).");
-						}
-					} catch(IOException o2) {
-						printErrorAndDie(o2.getMessage());
-					}
+			if(req.equals("LOGIN") && !isClientLoggedIn) try {
+				printError("Client at " + this.sock.getRemoteSocketAddress() + " logging in.");
+				//If session does not require password, send LOGIN_OK and continue:
+				if(this.passHash64==null) {
+					this.out.writeUTF("LOGIN_OK");
+					printError("Client at " + this.sock.getRemoteSocketAddress() + " logged in.");
+					this.out.flush();
+					isClientLoggedIn=true;
+					continue;
 				}
+				//Request password:
+				this.out.writeUTF("PASSWD");
+				//Check password:
+				String p=this.in.readUTF();
+				if(this.checkPassword(p)) {
+					//If ok, log client in and continue:
+					this.out.writeUTF("LOGIN_OK");
+					this.out.flush();
+					printError("Client at " + this.sock.getRemoteSocketAddress() + " logged in.");
+					isClientLoggedIn=true;
+				}
+				else {
+					//Else, reject login attempt and disconnect client:
+					this.out.writeUTF("LOGIN_BAD");
+					this.out.flush();
+					printErrorAndDie("Client at " + this.sock.getRemoteSocketAddress() + " disconnected (bad login).");
+				}
+			} catch(IOException o1) {
+				printErrorAndDie(o1.getMessage());
 			}
 			//Handle all other requests:
-			if(isClientLoggedIn) {
+			else try {
 				//Handle a LIST request:
 				if(req.equals("LIST")) {
-					try {
-						//Open directory at dirPath:
-						StringBuffer csvFileList=new StringBuffer();
-						File dir=new File(this.dirPath);
-						//Add files, exclude directories:
-						for(File f: dir.listFiles()) {
-							if(!(f.isDirectory()))
-								csvFileList.append(f + ",");
-						}
-						this.out.writeUTF(csvFileList.toString());
+					//Open directory at dirPath:
+					StringBuffer csvFileList=new StringBuffer();
+					File dir=new File(this.dirPath);
+					//Add files, exclude directories:
+					for(File f: dir.listFiles()) {
+						if(!(f.isDirectory()))
+							csvFileList.append(f + ",");
+					}
+					this.out.writeUTF(csvFileList.toString());
+					this.out.flush();
+					
+					//Wait for "RECV_OK":
+					String resp1=this.in.readUTF();
+					if(resp1.equals("RECV_OK")) {
+						this.out.writeUTF("LIST_OK");
 						this.out.flush();
-						
-						//Wait for "RECV_OK":
-						String resp1=this.in.readUTF();
-						if(resp1.equals("RECV_OK")) {
-							this.out.writeUTF("LIST_OK");
+					}
+					else {
+						this.out.writeUTF("NO_ACTION");
+						this.out.flush();
+					}
+				}
+				//Handle a DLOAD request:
+				else if(req.equals("DLOAD")) {
+					//Request file name:
+					this.out.writeUTF("FNAME");
+					this.out.flush();
+					//Open requested file:
+					String tempPath=this.dirPath + "/" + this.in.readUTF();
+					File tempFile=new File(tempPath);
+					if(!tempFile.exists()) {
+						this.out.writeUTF("DLOAD_NO_TARGET");
+						this.out.flush();
+						//continue
+					}
+					else {
+						this.out.writeUTF("FNAME_OK");
+						this.out.flush();
+						//Wait for "RECV":
+						String rep1=this.in.readUTF();
+						if(rep1.equals("RECV")) {
+							//Open file:
+							int fLen=(int)tempFile.length();
+							byte[] tempFileBytes=new byte[fLen];
+							//Read file data:
+							FileInputStream fin=new FileInputStream(tempFile);
+							fin.read(tempFileBytes, 0, fLen);
+							//Encode to base64:
+							String tempFile64=new String(Base64.getEncoder().encode(tempFileBytes), "UTF-8");
+							//Send data:
+							this.out.writeUTF(tempFile64);
 							this.out.flush();
+							//Wait for RECV_OK:
+							rep1=this.in.readUTF();
+							if(rep1.equals("RECV_OK")) {
+								this.out.writeUTF("DLOAD_OK");
+								this.out.flush();
+							}
 						}
 						else {
 							this.out.writeUTF("NO_ACTION");
 							this.out.flush();
 						}
-					} catch(IOException ie) {
-						printErrorAndDie(ie.getMessage());
-					}
-				}
-				//Handle a DLOAD request:
-				else if(req.equals("DLOAD")) {
-					try {
-						//Request file name:
-						this.out.writeUTF("FNAME");
-						this.out.flush();
-						//Open requested file:
-						String tempPath=this.dirPath + "/" + this.in.readUTF();
-						File tempFile=new File(tempPath);
-						printError(tempPath);
-						if(!tempFile.exists()) {
-							this.out.writeUTF("DLOAD_NO_TARGET");
-							this.out.flush();
-						}
-						else {
-							this.out.writeUTF("FNAME_OK");
-							this.out.flush();
-							//Wait for "RECV":
-							String rep1=this.in.readUTF();
-							if(rep1.equals("RECV")) {
-								//Read file data:
-								int fLen=(int)tempFile.length();
-								byte[] tempFileBytes=new byte[fLen];
-								FileInputStream fin=new FileInputStream(tempFile);
-								fin.read(tempFileBytes, 0, fLen);
-								//Encode to base64:
-								String tempFile64=new String(Base64.getEncoder().encode(tempFileBytes), "UTF-8");
-								//Send data:
-								this.out.writeUTF(tempFile64);
-								this.out.flush();
-								//Wait for RECV_OK:
-								rep1=this.in.readUTF();
-								if(rep1.equals("RECV_OK")) {
-									this.out.writeUTF("DLOAD_OK");
-									this.out.flush();
-									continue;
-								}
-								else {
-									this.out.writeUTF("NO_ACTION");
-									this.out.flush();
-									continue;
-								}
-							}
-							else {
-								this.out.writeUTF("NO_ACTION");
-								this.out.flush();
-								continue;
-							}
-						}
-					} catch(Exception ie) {
-						printErrorAndDie(ie.getMessage());
 					}
 				}
 				//Handle a ULOAD request:
 				else if(req.equals("ULOAD")) {
-					try {
-						//Request file name:
-						this.out.writeUTF("FNAME");
+					//Request file name:
+					this.out.writeUTF("FNAME");
+					this.out.flush();
+					//Read file name:
+					String tempPath=this.dirPath + "/" + this.in.readUTF();
+					File tempFile=new File(tempPath);
+					//Check if file exists:
+					if(tempFile.exists()) {
+						this.out.writeUTF("FNAME_CONFLICT");
 						this.out.flush();
-						//Read file name:
-						String tempPath=this.dirPath + "/" + this.in.readUTF();
-						File tempFile=new File(tempPath);
-						//Check if file exists:
-						if(tempFile.exists()) {
-							this.out.writeUTF("FNAME_CONFLICT");
-							this.out.flush();
-							continue;
-						}
-						//Receive file data (encoded as Base64):
-						this.out.writeUTF("FNAME_OK");
-						this.out.flush();
-						String tempData64=this.in.readUTF();
-						//Decode data:
-						byte[] tempData=Base64.getDecoder().decode(tempData64.trim().getBytes("UTF-8"));
-						//Write data to disk:
-						FileOutputStream fout=new FileOutputStream(tempFile);
-						try {
-							fout.write(tempData);
-						} catch(IOException e1) {
-							//In case of error:
-							this.out.writeUTF("ULOAD_BAD");
-							this.out.flush();
-							continue;
-						}
-						//Say everything went ok:
-						this.out.writeUTF("ULOAD_OK");
-						this.out.flush();
-					} catch(IOException ie) {
-						printErrorAndDie(ie.getMessage());
+						continue;
 					}
+					//Request file data (encoded as Base64):
+					this.out.writeUTF("FNAME_OK");
+					this.out.flush();
+					//Read file data:
+					String tempData64=this.in.readUTF();
+					//Decode data:
+					byte[] tempData=Base64.getDecoder().decode(tempData64.trim().getBytes("UTF-8"));
+					//Write data to disk:
+					FileOutputStream fout=new FileOutputStream(tempFile);
+					try {
+						fout.write(tempData);
+						fout.close();
+					} catch(IOException e1) {
+						//In case of error:
+						this.out.writeUTF("ULOAD_BAD");
+						this.out.flush();
+						printError("Warning: Disk I/O Error: " + e1.getMessage());
+						continue;
+					}
+					//Say everything went ok:
+					this.out.writeUTF("ULOAD_OK");
+					this.out.flush();
 				}
 				//Handle a DEL request:
 				else if(req.equals("DEL")) {
-					
+					//Request file name:
+					this.out.writeUTF("FNAME");
+					this.out.flush();
+					//Get file name:
+					String tempPath=this.dirPath + "/" + this.in.readUTF();
+					//Open file:
+					File tempFile=new File(tempPath);
+					if(!tempFile.exists()) {
+						//Send error if file does not exist:
+						this.out.writeUTF("DEL_NO_TARGET");
+						this.out.flush();
+						continue;
+					}
+					//Delete file:
+					tempFile.delete();
+					this.out.writeUTF("DEL_OK");
+					this.out.flush();
 				}
 				//Handle a LOGOUT request:
 				else if(req.equals("LOGOUT")) {
-					try {
-						printError("Client at " + this.sock.getRemoteSocketAddress() + " logging out.");
-						this.out.writeUTF("LOGOUT_OK");
-						this.out.flush();
-						this.out.close();
-						this.in.close();
-					} catch(IOException ie) {
-						printErrorAndDie(ie.getMessage());
-					}
+					printError("Client at " + this.sock.getRemoteSocketAddress() + " logging out.");
+					this.out.writeUTF("LOGOUT_OK");
+					this.out.flush();
+					this.out.close();
+					this.in.close();
 				}
 				else {
 					try {
@@ -275,6 +268,8 @@ public class ClientHandler implements Runnable {
 						printErrorAndDie(ie.getMessage());
 					}
 				}
+			} catch(IOException ie) {
+				printErrorAndDie(ie.getMessage());
 			}
 		}
 		printErrorAndDie("Connection closed.");
